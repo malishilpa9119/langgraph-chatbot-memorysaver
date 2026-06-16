c83ab8d5e01dddc32f8137416e9ddc9c8fe89fdd
Shilpa Mali <malishilpa05@gmail.com>
2026-06-16 16:53:50 +0530

    add database

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from langgraph_database_backend import chatbot, retrieve_all_threads
 from langchain_core.messages import HumanMessage
 import uuid
 
@@ -19,8 +19,19 @@ def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
         st.session_state['chat_threads'].append(thread_id)
 
+# def load_conversation(thread_id):
+#     return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
+
 def load_conversation(thread_id):
-    return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
+    state = chatbot.get_state(
+        config={'configurable': {'thread_id': thread_id}}
+    )
+
+    print("THREAD ID:", thread_id)
+    print("STATE:", state)
+    print("VALUES:", state.values)
+
+    return state.values.get("messages", [])
 
 # --------------------Session Setup-----------------------------
 # st.session_state -> dict -> 
@@ -31,7 +42,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
