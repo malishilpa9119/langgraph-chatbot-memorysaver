b6b6306f746d6bf360b2b14089ba2b209e26acff
Shilpa Mali <malishilpa05@gmail.com>
2026-07-09 16:18:58 +0530

    tool calling with mcp and add reg with tool

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -22,6 +22,7 @@ def add_thread(thread_id):
 # def load_conversation(thread_id):
 #     return chatbot.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
 
+
 def load_conversation(thread_id):
     state = chatbot.get_state(
         config={'configurable': {'thread_id': thread_id}}
@@ -88,7 +89,15 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    # CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+    CONFIG = {
+        "configurable": {'thread_id': st.session_state['thread_id']},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn"
+        }
 
     # first add the message to message history
     with st.chat_message('assistant'):
